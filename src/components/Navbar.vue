43cebca366bfddcfce8331a237bea198dccc6f65
RSlavov <rumen@eurocoders.com>
2024-08-21 12:26:03 +0300

    make task component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,34 +2,57 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
     <div class="container py-2">
       <a href="index.html" class="navbar-brand">
-        <span>Toedoe</span>
-        <strong>List</strong>
+        <span>Todo</span>
+        <strong>App</strong>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav" v-if="store.isLoggedIn">
           <li class="nav-item">
-            <a href="my-tasks.html" class="nav-link">Tasks</a>
+            <router-link :to="{ name: 'tasks' }" class="nav-link">Tasks</router-link>
           </li>
           <li class="nav-item">
-            <a href="summaries.html" class="nav-link">Summary</a>
+            <router-link :to="{ name: 'summary' }" class="nav-link">Summary</router-link>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a href="login.html" class="btn btn-outline-secondary ms-2">Login</a>
-          </li>
-          <li class="nav-item">
-            <a href="register.html" class="btn btn-danger ms-2">Register</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
-          </li>
+          <template v-if="!store.isLoggedIn">
+            <li class="nav-item">
+              <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
+            </li>
+          </template>
+          <template v-else>
+            <li class="nav-item">
+              <a href="#" class="btn btn-outline-secondary ms-2" @click.prevent="logout">Logout</a>
+            </li>
+          </template>
         </ul>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth.js";
+
+const router = useRouter();
+const store = useAuthStore()
+
+const logout = async () => {
+  await store.handleLogout()
+  router.push({ name: 'login' })
+}
+</script>
+
+<style scoped>
+  .navbar-light .navbar-nav .nav-link.active, .navbar-light .navbar-nav .show>.nav-link {
+    color: #000000;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
+import { createPinia } from "pinia";
+import routes from "@/router/index.js";
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(routes)
+app.mount('#app')
+
